3ddbeb36f3362f9e07ee2a021ba4fbbba037ce60
Michael Blondeau <michael-blondeau@laposte.net>
2021-07-12 11:14:52 +0200

    add boucle and function in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,43 @@
+//fetch de l'URL
 fetch("http://localhost:3000/api/teddies")
 .then(res => res.json())
 .then(response => displayProduct(response))
 .catch(error => alert("Erreur : " + error));
 
-function displayProduct (product) {
+// Fonction pour la création des produits
+function displayProduct (response) {
     //Boucle pour chaque produit
-    for (produit of product) {
-        
+    for (const produit of response) {
+      // Recupération de l'élément id dans le html
+      const card = document.getElementById("listing");
+      //Convertir le prix
+      const price = convertPrice(produit.price);
+      card.innerHTML += `
+      <a href="./pages/product.html?_id=${produit.id}">
+      <article>
+        <figure>
+          <p>${price}</p>
+          <img src="${produit.imageUrl}" class="img-fluid img-thumbnail p-1" alt="${produit.name}"/>     
+          <figcaption>
+            <h3>${produit.name}</h3>
+          </figcaption>
+        </figure>
+      </article>
+    </a>
+      ` 
       console.log(produit);  
     }
-    console.log(product)
+    
 
+}
+
+// convertisseur pour le prix en euro
+function convertPrice(productPrice) {
+  let price = `${productPrice}`;
+  price = Intl.NumberFormat("fr-FR", {
+      style: "currency",
+      currency: "EUR",
+      minimumFractionDigits: 2,
+  }).format(price / 100);
+  return price;
 }
