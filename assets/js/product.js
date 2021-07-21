8a2d6a2c33a5309fa1926df547954941f7c6dbe8
Michael Blondeau <michael-blondeau@laposte.net>
2021-07-21 10:32:19 +0200

    adding the quantity element

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,3 +1,4 @@
+// Importation de la fonction convertPrice
 import { convertPrice } from "./utils.js";
 const searchParams = new URLSearchParams(window.location.search);
 const cardId = searchParams.get("_id");
@@ -13,33 +14,34 @@ console.log(newUrl);
 function dataSheet(product) {
   // Recupération de l'élément id dans le html
   const productCard = document.getElementById("product");
-  //Boucle pour chaque produit
-
-  //Convertir le prix
-  /* const price = convertPrice(produit.price);*/
-  
+  //Boucle pour chaque produit  
   productCard.innerHTML += `
-        <h1>${product.name}</h1> 
-        <article>
-          <figure id="prod" class="d-flex">               
-        <div>
-          <img src="${product.imageUrl}" alt="${product.name}" />
-        </div>
-        <figcaption class="d-flex">
-          <p>
-            ${product.description}
-          </p>
-          <span>${convertPrice(product.price)}</span>
-        </figcaption>
-        </figure> 
-        <div id="customization" class="d-flex">
-            <label for="customization-select">Personnalisation:</label>
-            <select name="customization" id="customization-select">              
-            </select>
-          </div>
+    <h1>${product.name}</h1> 
+     <article>
+      <figure id="prod" class="d-flex">               
+       <div>
+        <img src="${product.imageUrl}" alt="${product.name}" />
+       </div>
+       <figcaption class="d-flex">
+        <p>
+         ${product.description}
+        </p>
+         <span>${convertPrice(product.price)}</span>
+       </figcaption>
+      </figure> 
+      <div id="customization" class="d-flex">
+       <label for="customization-select">Personnalisation:</label>
+        <select name="customization" id="customization-select"></select>
+      </div>
+      <div id="item-quantity" class="d-flex">
+       <label for="quantity">Quantité:</label>
+       <input type="number" id="quantity" name="quantity" min="0">
+      </div>
+
           <button id="addToCart">Ajouter au panier</button>
         </article>       
       `
+  // Ajout d'un tableau qui récupère les couleurs
   let optionColor = [];
   const select = product.colors;
   const selectOption = document.getElementById("customization-select");
@@ -51,11 +53,11 @@ function dataSheet(product) {
         </select>      
       </div>    
       `
+  // Boucle qui récupère les valeurs du tableau puis l'affiche    
   for (optionColor of select) {
     selectOption.innerHTML += `                
         <option value ="">${optionColor}</option>        
         `
-  }
-  
+        }  
 }
 
